9506a31cce237f0581b3f657d2fc1f5370629545
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-06-27 05:11:13 +0300

    typo

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -17,13 +17,13 @@ def detect_weather():
 
 
 if __name__ == "__main__":
-    detect_weather()
-    # weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
-    # if weather_input == "sunny":
-    #     print("Wear a t-shirt and sunglasses.")
-    # elif weather_input == "rainy":
-    #     print("Don't forget your umbrella and a raincoat.")
-    # elif weather_input == "cold":
-    #     print("Make sure to wear a warm coat and a scarf.")
-    # else:
-    #     print("Sorry, I don't have recommendations for this weather.")
+    # detect_weather()
+    weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
+    if weather_input == "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    elif weather_input == "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+    elif weather_input == "cold":
+        print("Make sure to wear a warm coat and a scarf.")
+    else:
+        print("Sorry, I don't have recommendations for this weather.")
