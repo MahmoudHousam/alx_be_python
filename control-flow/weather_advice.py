03b18f31fb98d201ecd18a42f2617f94c72ff587
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-06-27 05:18:12 +0300

    return to previos logic

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,8 +6,12 @@ def detect_weather():
     }
 
     weather = input("What's the weather like today? (sunny/rainy/cold): ")
-    if weather in weather_dict.keys():
-        print(weather_dict[weather])
+    if weather == "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    elif weather == "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+    elif weather == "cold":
+        print("Make sure to wear a warm coat and a scarf.")
     else:
         print("Sorry, I don't have recommendations for this weather.")
 
