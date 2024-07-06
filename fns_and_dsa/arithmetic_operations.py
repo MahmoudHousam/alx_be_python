82dcc08d340d10c00b45cdfd5b65b9049197d10b
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-06 13:46:57 +0300

    back to if/elif statements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,18 +3,17 @@
 # ):
 def perform_operation(num1, num2, operation):
     result = None
-    match operation:
-        case "add":
-            result = num1 + num2
-        case "subtract":
-            result = num1 - num2
-        case "multiply":
-            result = num1 * num2
-        case "divide":
-            try:
-                result = num1 / num2
-            except ZeroDivisionError:
-                print("Cannot divide by zero")
-        case _:
-            raise ValueError(f"Invalid operation {operation}")
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            print("Cannot divide by zero")
+    else:
+        raise ValueError(f"Invalid operation {operation}")
     return result
