f2c0c8a0277d0df23f0d49edc5e1f4a31b4eb8a6
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-06 13:43:38 +0300

    edits

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,7 @@
-def perform_operation(
-    num1: float, num2: float, operation={"add", "subtract", "multiply", "divide"}
-):
+# def perform_operation(
+#     num1: float, num2: float, operation={"add", "subtract", "multiply", "divide"}
+# ):
+def perform_operation(num1, num2, operation):
     result = None
     match operation:
         case "add":
