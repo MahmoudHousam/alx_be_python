9dcfd0c96836772f6a5151c5f72b62bef6083344
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-06 13:32:27 +0300

    perform operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(
+    num1: float, num2: float, operation={"add", "subtract", "multiply", "divide"}
+):
+    result = None
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            try:
+                result = num1 / num2
+            except ZeroDivisionError:
+                print("Cannot divide by zero")
+        case _:
+            raise ValueError(f"Invalid operation {operation}")
+    return result
