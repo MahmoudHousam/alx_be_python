ab907dcd2722adc524b06b60abddbea0635a8a95
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-13 02:23:30 +0300

    practicing OOP P.1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+
+    def __init__(self, account_balance=0) -> None:
+        self.account_balance = account_balance
+
+    def deposit(self, amount: float):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self, amount: float):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+        return self.account_balance
+
+    def display_balance(self):
+        print(f"{self.account_balance}")
