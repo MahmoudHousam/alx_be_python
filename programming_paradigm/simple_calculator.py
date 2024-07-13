341d975d6a581e92868490f0c84dd1cd2c1c403d
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-13 03:20:05 +0300

    practicing OOP P.3

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,16 @@
+class SimpleCalculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        """Return the subtraction of b from a."""
+        return a - b
+
+    def multiply(self, a, b):
+        """Return the multiplication of a and b."""
+        return a * b
+
+    def divide(self, a, b):
+        """Return the division of a by b. Returns None if b is zero."""
+        return a / b
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+import pytest
+from simple_calculator import SimpleCalculator
+
+
+class test_simple_calculator_class(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        with pytest.raises(TypeError):
+            self.calc.add("2", 4)
+        self.assertEqual(self.calc.subtract(13, 4), 9)
+        self.assertEqual(self.calc.multiply(4, 9), 36)
+        self.assertEqual(self.calc.divide(36, 9), 4)
+        with pytest.raises(ZeroDivisionError):
+            self.calc.divide(36, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
