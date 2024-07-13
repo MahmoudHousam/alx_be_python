08d686c0d9f04307fd1527b1542da7f9a5648626
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-13 03:24:08 +0300

    fix for test 5

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,7 +16,7 @@ class test_simple_calculator_class(unittest.TestCase):
         with pytest.raises(TypeError):
             self.calc.add("2", 4)
 
-    def test_subraction(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(13, 4), 9)
         with pytest.raises(TypeError):
             self.calc.subtract("2", 4)
