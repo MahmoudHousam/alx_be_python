247b85e549bb3fcfa005eec2e70d0b23c7367d78
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-14 03:48:43 +0300

    OOP P.4

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title, author) -> None:
+        self.title = title
+        self.author = author
+        # self._is_checked_out
+
+
+class Library:
+    def __init__(self) -> None:
+        self._books = []
+        self._authors = []
+
+    def add_book(self, Book):
+        self._books.append(Book.title)
+        self._authors.append(Book.author)
+
+    def list_available_books(self):
+        for book, author in zip(self._books, self._authors):
+            print(f"{book} by {author}")
+
+    def check_out_book(self, book):
+        if book in self._books:
+            self._books.remove(book)
+
+    def return_book(self, book):
+        if book not in self._books:
+            self._books.append(book)
