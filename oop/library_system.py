52cf5b0ece13032da11617b02d263079e65bc0bd
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-07-21 23:37:16 +0300

    fix for test 3

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,30 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self) -> str:
+        return "Book: {} by {}".format(self.title, self.author)
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size) -> None:
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self) -> str:
+        return "EBook: {} by {}, File Size: {}KB".format(
+            self.title, self.author, self.file_size
+        )
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count) -> None:
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self) -> str:
+        return "PrintBook: {} by {}, Page Count: {}".format(
+            self.title, self.author, self.page_count
+        )
 
 
 class Library:
